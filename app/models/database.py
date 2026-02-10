858b3e487356323e4fd235f1cc5d1357a6234a1b
Ecnamor <joan71300@gmail.com>
2026-02-10 09:28:01 +0100

    Fix database connection for PostgreSQL

--- app/models/database.py
+++ app/models/database.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
+connect_args = {"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
 engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.DATABASE_URL, connect_args=connect_args
+)
+
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
